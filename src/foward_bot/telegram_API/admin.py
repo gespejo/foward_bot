fa75a6270765fc42a27d47c853815e26f835069c
Kenneth Chinedu <kchinedu@mail.ru>
2018-11-07 20:01:55 +0300

    add search feature to chat admin

--- src/foward_bot/telegram_API/admin.py
+++ src/foward_bot/telegram_API/admin.py
@@ -5,9 +5,15 @@ from django.contrib import admin
 
 from .models import *
 
-#
+
+class ChatAdmin(admin.ModelAdmin):
+
+    list_display = ('identifier', 'title', 'username', 'first_name', 'last_name')
+    search_fields = ('title', 'username', 'first_name', 'last_name')
+
+
 admin.site.register(User)
 admin.site.register(Update)
 admin.site.register(Bot)
 admin.site.register(Message)
-admin.site.register(Chat)
+admin.site.register(Chat, ChatAdmin)
